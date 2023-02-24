6c7654074ea5bf88397d93c7b70621df249cbfee
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-02-24 17:21:03 -0600

    Add files via upload

---
+++ grupoJose.py
@@ -0,0 +1,24 @@
+edad1=19
+edad2=18
+edad3=17
+edad4=17
+edad5=18
+alumno = "Jose Lisandro Ramos Dominguez - edad: 19 - sexo masculino"
+alumno += "Rocio Marbelly Moreno Erazo - edad: 17 - sexo femenino "
+alumno += "Nestor Eduardo Gomez Franco -edad:18 - sexo masculino "
+alumno  += "Fatima Estefany Rivas Lopez - edad:18 - sexo femenino "
+alumno += "Xavier Ernesto Rivera Gonzalez - edad:17 - sexo masculino "
+
+edadfinal = edad1+5
+edadfinal2 = edad2+5
+edadfinal3 = edad3+5
+edadfinal4 = edad4 +5 
+edadfinal5 = edad5 +5
+
+print(alumno)
+print("la edad promedio de los alumnos es ", (edad1+edad2+edad3+edad4+edad5)/5)
+print("La edad de Jose alumno al terminar la carrera sera de ", edadfinal)
+print("La edad de Marbelly alumno al terminar la carrera sera de ", edadfinal2)
+print("La edad de Nestor alumno al terminar la carrera sera de ", edadfinal3)
+print("La edad de Fatima alumno al terminar la carrera sera de ", edadfinal4)
+print("La edad de Xavier alumno al terminar la carrera sera de ", edadfinal5)
